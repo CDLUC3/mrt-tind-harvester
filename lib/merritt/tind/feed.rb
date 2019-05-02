e6d85b42e9c7dc1e98514ac523125836e64a65c4
David Moles <david.moles@ucop.edu>
2019-05-02 13:43:37 -0700

    Implement pagination

--- lib/merritt/tind/feed.rb
+++ lib/merritt/tind/feed.rb
@@ -6,14 +6,22 @@ module Merritt
       include Enumerable
 
       def initialize(resp)
-        @resp = resp
+        @resp = ensure_full_response(resp)
       end
 
       def each
         return enum_for(:each) unless block_given?
 
-        # TODO: resumption tokens?
         @resp.each { |oai_record| yield Record.new(oai_record) }
+      end
+
+      private
+
+      def ensure_full_response(resp)
+        return resp unless resp.respond_to?(:resumption_token) # already wrapped
+        return resp unless resp.resumption_token # nothing to paginate
+
+        resp.full
       end
 
     end
--- spec/unit/merritt/tind/feed_spec.rb
+++ spec/unit/merritt/tind/feed_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'rexml/document'
+require 'webmock/rspec'
 
 module Merritt::TIND
   describe Feed do
@@ -26,6 +27,36 @@ module Merritt::TIND
             expect(count).to eq(expected_ids.size)
           end
         end
+      end
+    end
+
+    describe 'pagination' do
+      before(:each) do
+        WebMock.disable_net_connect!
+        # 'I am a resumption token' -> UTF-8 bytes -> big integer (little-endian) -> base36
+        token = '3kt58j5stglp50mv6wlrbgf7xyl6e2prrebt'
+        url1 = 'https://tind.example.edu/oai2d?verb=ListRecords&metadataPrefix=oai_dc&set=calher130'
+        stub_request(:get, url1).to_return(status: 200, body: File.new('spec/data/feed-1.xml'))
+        url2 = "https://tind.example.edu/oai2d?resumptionToken=#{token}&verb=ListRecords"
+        stub_request(:get, url2).to_return(status: 200, body: File.new('spec/data/feed-2.xml'))
+      end
+
+      after(:each) do
+        WebMock.allow_net_connect!
+      end
+
+      it 'handles resumption tokens' do
+        client = OAI::Client.new('https://tind.example.edu/oai2d')
+        resp = client.list_records(set: 'calher130')
+        feed = Feed.new(resp)
+        expected_ids = (5541..5565).map { |i| "oai:berkeley-test.tind.io:#{i}" }
+        count = 0
+        feed.each_with_index do |r, i|
+          expected_id = expected_ids[i]
+          expect(r.identifier).to eq(expected_id)
+          count += 1
+        end
+        expect(count).to eq(expected_ids.size)
       end
     end
   end
