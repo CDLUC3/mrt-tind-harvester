2bda9fd367c01ccec1a05177e7136e660b83c00d
David Moles <david.moles@ucop.edu>
2019-05-13 14:24:47 -0700

    Add Files.with_lock

--- spec/unit/merritt/tind/files_spec.rb
+++ spec/unit/merritt/tind/files_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'English'
 
 module Merritt::TIND
   describe Files do
@@ -34,6 +35,46 @@ module Merritt::TIND
         expect(actual_content).to eq(expected_content)
       end
 
+      it 'locks the file' do
+        filename = File.join(tmpdir, 'file.bin')
+        startfile = File.join(tmpdir, 'start.bin')
+        stopfile = File.join(tmpdir, 'stop.bin')
+
+        locking_process_id = fork do
+          Files.with_lock(filename) do |_|
+            File.open(startfile, 'w') { |f| f.puts('start') }
+            loop { File.exist?(stopfile) ? break : sleep(0.1) }
+          end
+        end
+
+        contending_process_id = fork do
+          loop { File.exist?(startfile) ? break : sleep(0.1) }
+          File.open(filename, 'a+t') do |f|
+            begin
+              can_lock = f.flock(File::LOCK_EX | File::LOCK_NB)
+              test_passed = can_lock.is_a?(FalseClass)
+              exit(test_passed ? 0 : 1)
+            ensure
+              f.flock(File::LOCK_UN) # just in case
+            end
+          end
+        end
+        Process.wait(contending_process_id)
+        expect($CHILD_STATUS.exitstatus).to eq(0), "expected #{filename} to be locked, but was not"
+
+        File.open(stopfile, 'w') { |f| f.puts('stop') }
+        Process.wait(locking_process_id)
+        expect($CHILD_STATUS.exitstatus).to eq(0) # just to be sure
+
+        File.open(filename, 'a+t') do |f|
+          begin
+            can_lock = f.flock(File::LOCK_EX | File::LOCK_NB)
+            expect(can_lock).to eq(0), "expected #{filename} to be unlocked, but was locked"
+          ensure
+            f.flock(File::LOCK_UN)
+          end
+        end
+      end
     end
 
   end
