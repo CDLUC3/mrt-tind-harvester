c99abe44c7ea2524836c526bfb2f396d3e9229a1
David Moles <david.moles@ucop.edu>
2019-05-16 16:00:00 -0700

    Fix tests

--- spec/unit/merritt/tind/harvest_job_spec.rb
+++ spec/unit/merritt/tind/harvest_job_spec.rb
@@ -13,6 +13,7 @@ module Merritt::TIND
       attr_reader :logdev
       attr_reader :job
       attr_reader :harvester
+      attr_reader :feed
 
       before(:each) do
         @logdev = instance_double(Logger::LogDevice)
@@ -26,37 +27,40 @@ module Merritt::TIND
         config = job.config
         @harvester = instance_double(Harvester)
         allow(Harvester).to receive(:new).with(config).and_return(harvester)
+
+        @feed = instance_double(Feed)
+        allow(feed).to receive(:each)
       end
 
       it 'defaults to harvesting all records' do
         config = job.config
         config.instance_variable_set(:@last_harvest, LastHarvest.new)
-        expect(harvester).to receive(:harvest).with(from_time: nil, until_time: nil)
+        expect(harvester).to receive(:harvest).with(from_time: nil, until_time: nil).and_return(feed)
         job.process_feed!
       end
 
       it 'prefers an explicit start time, if set' do
         from_time = Time.now
-        expect(harvester).to receive(:harvest).with(from_time: from_time, until_time: nil)
+        expect(harvester).to receive(:harvest).with(from_time: from_time, until_time: nil).and_return(feed)
         job.process_feed!(from_time: from_time)
       end
 
       it 'accepts an explicit end time' do
         until_time = Time.now
-        expect(harvester).to receive(:harvest).with(hash_including(until_time: until_time))
+        expect(harvester).to receive(:harvest).with(hash_including(until_time: until_time)).and_return(feed)
         job.process_feed!(until_time: until_time)
       end
 
       it 'prefers the "oldest failed" time, if no explicit start time set' do
         lh = job.config.last_harvest
-        expect(harvester).to receive(:harvest).with(hash_including(from_time: lh.oldest_failed_datestamp))
+        expect(harvester).to receive(:harvest).with(hash_including(from_time: lh.oldest_failed_datestamp)).and_return(feed)
         job.process_feed!
       end
 
       it 'falls back to the "newest success" time, if no explicit start time or "oldest failed" set' do
         lh = job.config.last_harvest
         lh.instance_variable_set(:@oldest_failed, nil)
-        expect(harvester).to receive(:harvest).with(hash_including(from_time: lh.newest_success_datestamp))
+        expect(harvester).to receive(:harvest).with(hash_including(from_time: lh.newest_success_datestamp)).and_return(feed)
         job.process_feed!
       end
     end
